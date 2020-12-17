3fec713c6944a1101ae2b47b11f6d86b890e18e2
zhangshuai <1923998527@qq.com>
2020-12-17 10:54:52 +0800

    zs

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import bofore from '@/components/bofore'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/bofore',
+      name: 'bofore',
+      component: bofore
     }
   ]
 })
